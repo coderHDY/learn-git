afc0f375043cce6b0e3f8da8f42f8b96901a85cb
coderHDY <69615213+coderHDY@users.noreply.github.com>
2021-08-11 06:59:41 +0800

    add version3

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 console.log("version1");
 console.log('other modify');
 console.log('version2');
+console.log('version3');
 console.log('小绿更改');
 console.log("我在小绿更改后更改，希望保留小绿");
 console.log('我在小黑之前的更改');
